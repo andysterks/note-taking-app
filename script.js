0cdbee027789ff62fbcbf17d316f4ad391c3bd75
Andy Sterkowitz <andy@andysterkowitz.com>
2022-01-01 19:02:06 -0600

    Styling changes. Adds shift plus enter functionality

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const createNoteButton = document.getElementById('create-note-button');
+document.addEventListener('keydown', e => {
+  if (e.shiftKey) {
+    if (e.key === 'Enter') {
+      addNewNote();
+    }
+  }
+});
 
+const createNoteButton = document.getElementById('create-note-button');
 createNoteButton.addEventListener('click', e => {
+  addNewNote();
+});
+
+function addNewNote() {
+  const emptyNote = document.querySelector("#empty-note");
+  if (emptyNote) {
+    emptyNote.parentElement.removeChild(emptyNote);
+  }
+
   const note = document.createElement('div');
   note.classList.add('note');
 
+  const noteTitle = document.createElement('h3');
+  noteTitle.textContent = new Date().toLocaleString();
+  note.appendChild(noteTitle);
+
   const noteText = document.createElement('p');
-  noteText.textContent = document.querySelector('#note-textarea').value;
+  const noteTextArea = document.getElementById('note-textarea');
+  noteText.textContent = noteTextArea.value;
   note.appendChild(noteText);
 
   document.querySelector('#notes-list').appendChild(note);
-});
+
+  noteTextArea.value = '';
+  noteTextArea.focus();
+}
